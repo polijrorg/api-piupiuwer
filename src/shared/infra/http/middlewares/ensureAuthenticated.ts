89ff90172c9c0f2df0d9cab5f5220dbd99b822c5
Gabriel Mascarenhas <gfmascarenhas@gmail.com>
2021-02-19 19:25:08 -0300

    add: added ensureAuthenticated middleware

---
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,32 @@
+import auth from 'config/auth';
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import AppError from '@shared/errors/AppError';
+
+interface ITokenPayload {
+  sub: string;
+  exp: number;
+}
+
+export default function ensureAuthenticated(request: Request, response: Response, next: NextFunction): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) { throw new AppError('Missing JWT token'); }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, auth.jwt.secret);
+
+    const { sub } = decoded as ITokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
+    return next();
+  } catch (error) {
+    throw new AppError('Invalid JWT token');
+  }
+}
