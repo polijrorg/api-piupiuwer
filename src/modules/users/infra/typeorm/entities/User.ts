11b95df28fa8b27ce9aa26e6e53d6821eb7d29cc
Gabriel Mascarenhas <gfmascarenhas@gmail.com>
2021-02-19 21:41:40 -0300

    add: added pius relations to user

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -1,6 +1,9 @@
 import {
-  Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn,
+  Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn,
 } from 'typeorm';
+
+import Piu from '@modules/pius/infra/typeorm/entities/Piu';
+import PiuLike from '@modules/pius/infra/typeorm/entities/PiuLike';
 
 @Entity('users')
 class User {
@@ -27,6 +30,12 @@ class User {
 
   @Column()
   password: string;
+
+  @OneToMany(() => Piu, (piu) => piu.user)
+  pius: Piu[];
+
+  @OneToMany(() => PiuLike, (piuLike) => piuLike.user)
+  likes: PiuLike[];
 
   @CreateDateColumn()
   created_at: Date;
